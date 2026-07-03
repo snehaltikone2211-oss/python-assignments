aa4af803faa5510446d7ddf403a861c67c3d5419
snehaltikone2211-oss <snehaltikone2216@gmail.com>
2026-07-03 14:17:19 +0530

    Add files via upload

---
+++ Assignment9..2.py
@@ -0,0 +1,9 @@
+a = 10
+b = 20
+def ChkGreater(a,b):
+    if a > b:
+        print(a)
+    else:
+        print(b)
+
+ChkGreater(a,b)
---
+++ Assignment9.5.py
@@ -0,0 +1,6 @@
+a = int(input("Enter a number:"))
+
+if a % 3 == 0 and a % 5 == 0:
+    print("Divisible by 3 and 5")
+else:
+    print("Not divisible")
